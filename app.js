eec20515ecc069f55ef8610dccf4c29520dd3463
Mack Miller <mack.matt.miller@gmail.com>
2018-01-11 18:53:25 -0600

    tweaks

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // Set up =====================================================================
-var express          = require('express');
-var path             = require('path');
-var favicon          = require('serve-favicon');
-var logger           = require('morgan');
-var cookieParser     = require('cookie-parser');
-var bodyParser       = require('body-parser');
-var expressValidator = require('express-validator');
-var mongoose         = require('mongoose');
-var passport         = require('passport');
-var flash            = require('connect-flash');
-var session          = require('express-session');
-var compression      = require('compression');
-var helmet           = require('helmet');
+var express = require("express");
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
+var mongoose = require("mongoose");
+var passport = require("passport");
+var flash = require("connect-flash");
+var session = require("express-session");
+var compression = require("compression");
+var helmet = require("helmet");
 
-var configDB         = require('./config/database.js');
+var configDB = require("./config/database.js");
 
 // Configuration ==============================================================
 mongoose.connect(configDB.url, {
@@ -23,43 +23,43 @@ mongoose.connect(configDB.url, {
 });
 mongoose.Promise = global.Promise;
 var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var polls = require('./routes/polls');
+var index = require("./routes/index");
+var users = require("./routes/users");
+var polls = require("./routes/polls");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
 app.use(helmet());
-app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // required for passport
-app.use(session({ secret: 'findingdory' }));
+app.use(session({ secret: "findingdory" }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 
-app.use('/', index);
-app.use('/users', users);
-app.use('/polls', polls);
+app.use("/", index);
+app.use("/users", users);
+app.use("/polls", polls);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
@@ -68,11 +68,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
--- config/passport.js
+++ config/passport.js
@@ -1,77 +1,96 @@
 // load all the things we need
-var LocalStrategy = require('passport-local').Strategy;
+var LocalStrategy = require("passport-local").Strategy;
 
 // load up the user model
-var User = require('../models/User');
+var User = require("../models/User");
 
 // expose this function to our app using module.exports
 module.exports = function(passport) {
-	// Passport session setup
+  // Passport session setup
 
-	// Serialize the user for the sesion
-	passport.serializeUser(function(user, done) {
-		done(null, user.id);
-	});
+  // Serialize the user for the sesion
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
 
-	// Deserialize the user
-	passport.deserializeUser(function(id, done) {
-		User.findById(id, function(err, user) {
-			done(err, user);
-		});
-	});
-	
-	// ========================================================================
-	// Local Signup
-	// ========================================================================
+  // Deserialize the user
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 
-	passport.use('local-signup', new LocalStrategy({
-		// by default, local strategy uses username and password, we will override with email
-		usernameField: 'name',
-		passwordField: 'password',
-		passReqToCallback: true
-	},
-	function(req, name, password, done) {
-		process.nextTick(function() {
-			User.findOne({ 'name': name}, function(err, user) {
-				if (err)
-					return done(err);
+  // ========================================================================
+  // Local Signup
+  // ========================================================================
 
-				if (user) {
-					return done(null, false, req.flash('signupMessage', 'That email is already taken'));
-				} else {
-					var newUser = new User();
+  passport.use(
+    "local-signup",
+    new LocalStrategy(
+      {
+        // by default, local strategy uses username and password, we will override with email
+        usernameField: "name",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      function(req, name, password, done) {
+        process.nextTick(function() {
+          User.findOne({ name: name }, function(err, user) {
+            if (err) return done(err);
 
-					newUser.name = name;
-					newUser.password = newUser.generateHash(password);
+            if (user) {
+              return done(
+                null,
+                false,
+                req.flash("signupMessage", "That email is already taken")
+              );
+            } else {
+              var newUser = new User();
 
-					newUser.save(function(err) {
-						if (err)
-							throw err;
-						return done(null, newUser);
-					});
-				}
-			});
-		});
-	}));
+              newUser.name = name;
+              newUser.password = newUser.generateHash(password);
 
-	// ========================================================================
-	// Local Login
-	// ========================================================================
+              newUser.save(function(err) {
+                if (err) throw err;
+                return done(null, newUser);
+              });
+            }
+          });
+        });
+      }
+    )
+  );
 
-	passport.use('local-login', new LocalStrategy({
-		usernameField : 'name',
-		passwordField : 'password',
-		passReqToCallback : true
-	},
-	function(req, name, password, done) {
-		User.findOne({ 'name' : name }, function(err, user) {
-			if (err)
-				return done(err);
-			if (!user)
-				return done(null, false, req.flash('loginMessage', 'No user found.'));
-			if (!user.validPassword(password))
-				return done(null, false, req.flash('loginMessage', 'Oops! Wrong password.'));
-			return done(null, user);
-		});
-	}));
+  // ========================================================================
+  // Local Login
+  // ========================================================================
+
+  passport.use(
+    "local-login",
+    new LocalStrategy(
+      {
+        usernameField: "name",
+        passwordField: "password",
+        passReqToCallback: true
+      },
+      function(req, name, password, done) {
+        User.findOne({ name: name }, function(err, user) {
+          if (err) return done(err);
+          if (!user)
+            return done(
+              null,
+              false,
+              req.flash("loginMessage", "No user found.")
+            );
+          if (!user.validPassword(password))
+            return done(
+              null,
+              false,
+              req.flash("loginMessage", "Oops! Wrong password.")
+            );
+          return done(null, user);
+        });
+      }
+    )
+  );
 };
